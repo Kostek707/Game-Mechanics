04189b15529949f80c47fb7d11e41578683b3d11
Kostek707 <m.kostrzewa707@gmail.com>
2020-07-15 02:04:46 +0200

    Endless Run

---
+++ Endless Runner/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D myBody;
+    private bool isJumping;
+    public float jumpForce;
+
+    public Transform DownCollsion;
+    public LayerMask groundLayer;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
+
+
+    private Animator anim;
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        isJumping = false;
+
+        jumpTimeCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&!isJumping)
+        {
+            myBody.velocity = Vector2.up * jumpForce;
+            isJumping = true;
+
+            anim.Play("DinoIdle");
+
+            jumpTimeCounter = jumpTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.Space)&&jumpTimeCounter>0&&!isJumping)
+        {
+            myBody.velocity = Vector2.up * jumpForce;
+        }
+
+        if (Physics2D.Raycast(DownCollsion.position, Vector2.down, 0.05f, groundLayer))
+        {
+            isJumping = false;
+            anim.Play("DinoRun");
+        }
+
+        jumpTimeCounter -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Time.timeScale = 0;
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    private float speed_argument = 4;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 2f));
+
+        GameObject Obstacle_initiated = Instantiate(obstacle, transform.position, Quaternion.identity);
+
+        StartCoroutine(Spawn());
+
+    }
+}
